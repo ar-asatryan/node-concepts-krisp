ab5cc03220fa9bd1242e41f7661424200d2a0066
ar-asatryan <ar.asatryan@gmail.com>
2022-02-15 12:09:27 +0400

    feat(app): middleware next

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
 const express = require("express");
 const app = express();
 
+const posts = [
+  {
+    username: "john",
+    age: 15
+  }, {
+    username: "george",
+    age: 23
+  }
+]
 
-app.use( (req, res, next) => {
-    res.status(200).json({
-        message: "Server Success >>>> !!!"
-    })
+app.use(logger)
 
-    next();
+app.get('/', (req, res, next) => {
+  console.log("HOME PAGE");
+  res.send("HOME PAGE ...")
+  next()
+})
+
+app.get('/users', auth, (req, res, next) => {
+    console.log("USERS logging ...");
+    res.send("USERS PAGE ...")
+    next()
+})
+
+app.get( '/posts', (req, res, next) => {
+    console.log("Logging POSTS....");
+    res.status(200).json(posts)
+    next()
 })
 
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "Other Message !!",
-  });
-});
+
+function logger (req, res, next) {
+
+  console.log("Logging...")
+  next()
+
+}
+
+function auth (req, res, next) {
+
+  console.log("Auth  Users...")
+  next()
+
+}
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-console.log("Greetings Nodejs");
+// display()
 
-console.log("Testing the Nodemon...");
+// var strFirst = "Greetings!!!!"
 
+// display()
+
+// function display() {
+//     console.log("strFirst", strFirst);
+    
+// }
+
+
+// setTimeout( () => console.log("Inside the SetTimeOut"), 1000)
+
+
+// Promise.resolve().then( () => console.log("Inside the Promise....") )
+
+
+// while(true) {
+//     console.log("Inside the Loop.....");
+// }
+
+
+const users = [
+    { id: 1, name: "tony", surname: "stark", age: 45, cool: true , budget: 50200, email: "stark@gmail.com"},
+    { id: 2, name: "steve", surname: "strange", age: 40, cool: true, budget: 45850, email: "strange@gmail.com" },
+    { id: 3, name: "bruce", surname: "banner", age: 35, cool: false, budget: 30900, email: "banner@gmail.com" },
+    { id: 4, name: "steve", surname: "rogers", age: 73, cool: false, budget: 15050, email: "rogers@gmail.com" },
+    { id: 5, name: "sia", surname: "yung", age: 33, cool: true, budget: 22200, email: "yung@gmail.com" },
+    { id: 6, name: "mystic", surname: "man", age: 30, cool: false, budget: 19998, email: "man@gmail.com" },
+  ];
+
+
+//   Array.prototype.myMap = function(callback, thisVal = this) {
+//     const newArray = [];
+//     for (let i = 0; i < thisVal.length; i++) {
+//         newArray.push(callback(thisVal[i], i, thisVal));
+//     }
+//     return newArray;
+// }
+
+Array.prototype.myMap = function(callback, thisVal = this) {
+    const reducer = (acc, val, i, thisArg) => {
+        acc.push(callback(val, i, thisArg))
+        return acc;
+    }
+    return thisVal.reduce(reducer, [])
+}
+
+const mappedUsers = users.myMap( user => ({
+    id: user.id,
+    age: user.age,
+    fullname: `${user.name} ${user.surname}`
+}) )
+
+// Array.prototype.myMap = function(callback, thisVal) {
+//     const newArray = [];
+//     for (let i = 0; i < this.length; i++) {
+//         newArray.push(callback(this[i], i, this));
+//     }
+//     return newArray;
+// }
+
+console.log(mappedUsers);
